feddae98d1779f4d577c6c1dec5f33f6d230180d
anemocod <dokuchaev.andrey.nikolaevich@gmail.com>
2023-04-09 19:32:02 +0000

    Create server for deploying model

---
+++ server.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+import numpy as np
+import pickle
+
+with open('model/model.pkl', 'rb') as input:
+    model = pickle.load(input)
+
+app = Flask(__name__)
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+
+    features = request.json
+    features = np.array([features])
+    prediction = model.predict(features)
+    prediction = prediction[0]
+    return jsonify({'prediction': prediction})
+
+
+if __name__ == '__main__':
+
+    app.run('localhost', 5000)
